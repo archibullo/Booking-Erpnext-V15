5fe84b9555e97eec5b4fdaf47bac376b04fb7c2b
archibullo <archibullo@gmail.com>
2024-11-12 15:06:08 +0000

    Updating to Version 15 Erpnext

---
+++ booking/room_management/doctype/extras/test_extras.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Abdelkader Elhaidary and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestExtras(FrappeTestCase):
+	pass
